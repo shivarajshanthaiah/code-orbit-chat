1e7e0104b554f7ec0cfd5ffba84337ce0463bbda
shivarajshanthaiah <shivarajshanthaiah@gmai.com>
2024-10-19 16:44:46 +0530

    added chat history fetching

--- pkg/repo/chat.go
+++ pkg/repo/chat.go
@@ -20,28 +20,28 @@ func (r *MongoRepository) Createchat(chat *models.Message) error {
 	return nil
 }
 
-
 func (r *MongoRepository) Findchat(userID, receiverID string) (*[]models.Message, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
 
-	filter := bson.M{
-		"$or": []bson.M{
-			{"senderid": userID, "recipientid": receiverID},
-			{"senderid": receiverID, "recipientid": userID},
-		},
-	}
-	opts := options.Find().SetSort(bson.D{{Key: "timestamp", Value: 1}})
-	cursor, err := r.Collection.Find(ctx, filter, opts)
-	if err != nil {
-		return nil, err
-	}
-	defer cursor.Close(ctx)
+    filter := bson.M{
+        "$or": []bson.M{
+            {"senderid": userID, "recipientid": receiverID},
+            {"senderid": receiverID, "recipientid": userID},
+        },
+    }
+ 
+    opts := options.Find().SetSort(bson.D{{Key: "timestamp", Value: -1}})
+    cursor, err := r.Collection.Find(ctx, filter, opts)
+    if err != nil {
+        return nil, err
+    }
+    defer cursor.Close(ctx)
 
-	var messages []models.Message
-	if err := cursor.All(ctx, &messages); err != nil {
-		return nil, err
-	}
+    var messages []models.Message
+    if err := cursor.All(ctx, &messages); err != nil {
+        return nil, err
+    }
 
-	return &messages, nil
+    return &messages, nil
 }
